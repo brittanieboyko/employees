2885149b25655ab633f1edeef3610a1b40a80204
brittanieb <brittanie.boyko@gmail.com>
2020-04-10 21:35:54 -0400

    removed unneeded boilerplate code

--- src/App.js
+++ src/App.js
@@ -1,29 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import API from './utils/API';
 
 function App() {
-  const getDog = () => { API.getRandomUser().then((data) => {
-    console.log(data.data.results)
-  })};
-  getDog()
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
