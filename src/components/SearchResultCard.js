a495a1109c2d113d1a4d2760e584ebbe3358a707
brittanieb <brittanie.boyko@gmail.com>
2020-04-11 16:12:38 -0400

    updated search results to display a table

--- src/components/SearchResultCard.js
+++ src/components/SearchResultCard.js
@@ -1,21 +1,32 @@
 import React from "react";
 
 function SearchResultCard(props) {
-    console.log("props:", props)
+  console.log(props)
   return (
-    <ul className="list-group">
-      {props.results.map(result => (
-        <li className="list-group-item" key={result.login.uuid}>
-            <img alt="Employee" className="img-fluid" src={result.picture.thumbnail} />
-            <span>{result.name.first} {result.name.last}</span>
-            <span>{result.email}</span>
-            <span>{result.dob.date}</span>
-        </li>
-      ))}
-      </ul>
-    );
-  }
+    <table className="table">
+      <thead className="thead-dark">
+        <tr>
+          <th scope="col"></th>
+          <th scope="col">Name</th>
+          <th scope="col">Email Address</th>
+          <th scope="col">Phone Number</th>
+          <th scope="col">Date of Birth</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.results.map(result => (
+          <tr key={result.login.uuid}>
+            <th scope="row"><img alt="Employee" className="img-fluid" src={result.picture.medium}/></th>
+            <td>{result.name.first} {result.name.last}</td>
+            <td>{result.email}</td>
+            <td>{result.cell}</td>
+            <td>{result.dob.date}</td>
+          </tr>
+        ))}
+    </tbody>
+  </table>
+  );
+}
 
 export default SearchResultCard;
-
 
