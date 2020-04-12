4d68c69d72e461fc36da297ed6c53f9ba6a3e1c5
brittanieb <brittanie.boyko@gmail.com>
2020-04-12 16:39:09 -0400

    moved rendering of searchResultCard component from container to search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,19 +1,24 @@
 import React from 'react';
+import SearchResultCard from "./SearchResultCard";
 
-function Search() {
+function Search(props) {
     return (
         <div className="container">
             <form className="form-inline my-2 my-lg-0">
                 <input
-                    className="form-control mr-sm-2"
-                    type="search"
-                    placeholder="Search"
-                    aria-label="Search"
+                    onChange={props.handleInputChange}
+                    value={props.search}
+                    name="search"
+                    type="text"
+                    className="form-control"
+                    placeholder='Search...'
+                    id="search"
                 />
                 <button className="btn btn-outline-dark my-2 my-sm-0" type="submit">
                 Search
                 </button>
           </form>
+          <SearchResultCard results={props.results} />
         </div>
     )
 }
--- src/components/SearchResultCard.js
+++ src/components/SearchResultCard.js
@@ -2,29 +2,32 @@ import React from "react";
 
 function SearchResultCard(props) {
   console.log(props)
+
   return (
-    <table className="table">
-      <thead className="thead-dark">
-        <tr>
-          <th scope="col"></th>
-          <th scope="col">Name</th>
-          <th scope="col">Email Address</th>
-          <th scope="col">Phone Number</th>
-          <th scope="col">Date of Birth</th>
-        </tr>
-      </thead>
-      <tbody>
-        {props.results.map(result => (
-          <tr key={result.login.uuid}>
-            <th scope="row"><img alt="Employee" className="img-fluid" src={result.picture.medium}/></th>
-            <td>{result.name.first} {result.name.last}</td>
-            <td>{result.email}</td>
-            <td>{result.cell}</td>
-            <td>{result.dob.date}</td>
+    <div className="table-responsive-lg">
+      <table className="table">
+        <thead className="thead-dark">
+          <tr>
+            <th scope="col"></th>
+            <th scope="col">Name</th>
+            <th scope="col">Email Address</th>
+            <th scope="col">Phone Number</th>
+            <th scope="col">Date of Birth</th>
           </tr>
-        ))}
-    </tbody>
-  </table>
+        </thead>
+        <tbody>
+          {props.results.map(result => (
+            <tr key={result.login.uuid}>
+              <th scope="row"><img alt="Employee" className="img-fluid" src={result.picture.medium}/></th>
+              <td>{result.name.first} {result.name.last}</td>
+              <td>{result.email}</td>
+              <td>{result.cell}</td>
+              <td>{result.dob.date}</td>
+            </tr>
+          ))}
+      </tbody>
+    </table>
+  </div>
   );
 }
 
