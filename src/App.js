424a5b64606091e480d30ecb083c26e0fd3d2823
brittanieb <brittanie.boyko@gmail.com>
2020-04-12 16:39:57 -0400

    search  through api results with filter method

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-import Search from './components/Search';
 import SearchResultContainer from './components/SearchResultContainer';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Search />
       <SearchResultContainer />
     </div>
   );
--- src/components/SearchResultContainer.js
+++ src/components/SearchResultContainer.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import SearchResultCard from "./SearchResultCard";
+import Search from "./Search";
 import API from "../utils/API";
 
 class SearchResultContainer extends Component {
@@ -9,44 +9,41 @@ class SearchResultContainer extends Component {
   };
 
   componentDidMount() {
-    this.searchEmployee();
-  }
-
-  searchEmployee = () => {
     API.getRandomUser()
-      .then(data => {
-        this.setState({ results: data.data.results })
-      })
+      .then(data => { this.setState({ results: data.data.results })})
       .catch(err => console.log(err));
   };
 
-//   handleInputChange = event => {
-//     const name = event.target.name;
-//     const value = event.target.value;
-//     this.setState({
-//       [name]: value
-//     });
-//   };
+  handleInputChange = event => {
+    event.preventDefault();
+    if (event.target.name === "search") {
+      const searchTerm = event.target.value.toLowerCase();
+      this.setState({
+        search: searchTerm
+      })
+    }
+  }
 
-  // When the form is submitted, search the Giphy API for `this.state.search`
-//   handleFormSubmit = event => {
-//     event.preventDefault();
-//     this.searchGiphy(this.state.search);
-//   };
+  sortResults = (event) => {
+    event.preventDefault();
+    this.setState({results: this.state.results.sort()}) 
+  }
 
   render() {
+    let filteredEmployees = this.state.results.filter(
+      (employee) => {
+        return employee.name.first.toLowerCase().includes(this.state.search.toLowerCase());
+      })
     return (
       <div>
-        {/* <SearchForm
-          search={this.state.search}
-          handleFormSubmit={this.handleFormSubmit}
+        <Search
+          handleSort={this.sortResults}
           handleInputChange={this.handleInputChange}
-        /> */}
-        <SearchResultCard results={this.state.results} />
+          search={this.state.search}
+          results={filteredEmployees}/>
       </div>
     );
   }
 }
 
 export default SearchResultContainer;
-
